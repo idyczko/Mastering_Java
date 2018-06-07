3fc92aefbad8306992aead8701b7170f38a9a06c
Dyczko <igor.dyczko@capgemini.com>
2018-06-07 10:58:18 +0100

    Comment added for describing the experiment result.

--- Language_Features/abstractclasses/Main.java
+++ Language_Features/abstractclasses/Main.java
@@ -3,7 +3,12 @@ public class Main {
   public static void main(String[] args) {
     DerivedClass dc = new DerivedClass("Tobey");
     AbstractClass ac = dc;
-    System.out.println(dc.getName());
+    /*First opration has a astonishing result - it will produce "AbstractTobey".
+    It seems, that the method call is being delegated to base class, where "this"
+    means what "super" would mean in the child class. Therefore the field from
+    the base class is being read.
+    */
+    Sysyem.out.println(dc.getName());
     System.out.println(dc.name);
     System.out.println(ac.getName());
     System.out.println(ac.name);
