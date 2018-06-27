2250209c975ce60ce3130ab575b8ac5408889001
idyczko <igor.dyczko@gmail.com>
2018-06-27 22:22:47 +0200

    Rotating matrix in place.

--- Cracking_the_Coding_Interview/matrix/Main.java
+++ Cracking_the_Coding_Interview/matrix/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
   public static void main(String args[]) {
-    
+    //int matrix[][] = new int[][] {{1,2,3}, {1,2,3}, {1,2,3}};
+    //int matrix[][] = new int[][] {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1}};
+    int matrix[][] = new int[][] {{1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5}, {1,2,3,4,5}};
+    printMatrix(matrix);
+    int matrix2[][] = MatrixCalculator.rotateRight(matrix);
+    printMatrix(matrix2);
+  }
+
+  private static void printMatrix(int[][] matrix) {
+    for(int i = 0; i < matrix.length; i++) {
+      for(int j = 0; j < matrix.length; j++) {
+        System.out.print(matrix[j][i] + "\t");
+      }
+      System.out.println();
+    }
   }
 }
--- Cracking_the_Coding_Interview/matrix/MatrixCalculator.java
+++ Cracking_the_Coding_Interview/matrix/MatrixCalculator.java
@@ -1,12 +1,37 @@
 public class MatrixCalculator {
 
+  public static int[][] rotateRight(int[][] matrix) {
+    int size = matrix.length;
+    for(int i = 0; i <= (size-1)/2; i++) {
+      for(int j = 0; j < size/2; j++) {
+        Point p = new Point(i, j);
+        Point q = rotateLeft(size, p);
+        int temp = matrix[p.x][p.y];
+        matrix[p.x][p.y] = matrix[q.x][q.y];
+        p = q;
+        q = rotateLeft(size, q);
+        matrix[p.x][p.y] = matrix[q.x][q.y];
+        p = q;
+        q = rotateLeft(size, q);
+        matrix[p.x][p.y] = matrix[q.x][q.y];
+        matrix[q.x][q.y] = temp;
+      }
+    }
+    return matrix;
+  }
 
-  public static Point rotatePoint(int size, Point p) {
-    return new Point(){{x = size - p.y - 1; y = p.x;}};
+  public static Point rotateLeft(int size, Point p) {
+    return new Point(p.y, size - p.x -1);
+  }
+
+  public static Point rotateRight(int size, Point p) {
+    return new Point(size - p.y - 1, p.x);
   }
 
   public static class Point {
     public int x;
     public int y;
+
+    public Point(int x, int y) {this.x = x; this.y = y;}
   }
 }
