f04ae7fae8f321ca97dac59055ad4a7b8cd12f51
idyczko <igor.dyczko@gmail.com>
2018-08-17 18:13:11 +0200

    Partial solution to a GIQ.

---
+++ Google_Interview/Main.java
@@ -0,0 +1,66 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		System.out.println(eval("3"));
+		System.out.println(eval("( + 7 ( * 8 12 ) ( * 2 ( + 9 4 ) 7 ) 3 )"));
+	}
+
+	public static int eval(String expr) {
+		return eval(expr.toCharArray(), 0, expr.length());
+	}
+
+	public static int eval(char[] expression, int begin, int end) {
+		if (expression[begin] != '(')
+			return Integer.valueOf(new String(expression).substring(begin, end));
+		
+		if (expression[begin + 1] == '+') {
+			return sum(extractOperands(expression, begin + 4, end - 2));
+		}  else {
+			return product(extractOperands(expression, begin + 4, end - 2));
+		}
+	}
+
+	public static int[] extractOperands(char[] expression, int begin, int end) {
+		List<Integer> operands = new ArrayList<>();
+		System.out.println(new String(expression).substring(begin, end + 1));
+		while(begin < end) {
+			int endIndex = begin;
+			if (expression[begin] != '(') {
+				while(expression[++endIndex] != ' ');
+				endIndex--;
+			} else {
+				int brackets = 1;
+				while (brackets > 0) {
+					endIndex++;
+					if (expression[endIndex] == '(')
+						brackets++;
+					else  if (expression[endIndex] == ')')
+						brackets--;
+				}
+			}
+			System.out.println(begin + " " + endIndex + " " + new String(expression).substring(begin, endIndex + 1));
+			operands.add(eval(expression, begin, endIndex + 1));
+			begin = endIndex + 2;
+		}
+		return operands.stream().mapToInt(Integer::valueOf).toArray();
+	}
+
+	public static int sum(int... operands) {
+		return Arrays.stream(operands).sum();
+	}
+
+	public static int product(int... operands) {
+		int accu = 1;
+		for (int operand : operands) {
+			accu *= operand;
+		}
+		return accu;
+	}
+
+}
