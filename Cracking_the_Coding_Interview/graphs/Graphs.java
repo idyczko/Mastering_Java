ed9fe675b35122afa6e8e8389e3d02e0dd3cc394
Dyczko <igor.dyczko@capgemini.com>
2018-09-05 16:01:00 +0100

    Some work on bidirectional BF search.

--- Cracking_the_Coding_Interview/graphs/Graphs.java
+++ Cracking_the_Coding_Interview/graphs/Graphs.java
@@ -25,15 +25,45 @@ public class Graphs {
 	}
 
 	public static List<Integer> shortestPath(Graph g, int start, int end) {
-		LinkedList<BFSAncestryPreservingNode> path = treePreservingBFS(g, start, end);
+		LinkedList<BFSNode> path = treePreservingBFS(g, start, end);
 		return resolvePathFromTree(path);
 	}
+	
+	public static List<Integer> bidirectionalSearchPath(Graph g, int start, int end) {
+		Optional<Graph.Node> startNode = g.nodes().stream().filter(item -> item.id == start).findFirst();
+		Optional<Graph.Node> endNode = g.nodes().stream().filter(item -> item.id == end).findFirst();
+		LinkedList<BFSNode> pathHead = new LinkedList<>();
+		LinkedList<BFSNode> pathTail = new LinkedList<>();
+					       
+		if (!startNode.isPresent() || !endNode.isPresent())
+			throw new IllegalArgumentException("Either start or end node could not be found in the graph.");
 
-	private static List<Integer> resolvePathFromTree(LinkedList<BFSAncestryPreservingNode> tree) {
+		Set<Graph.Node> visitedFromStart = new HashSet<>();	
+		Set<Graph.Node> visitedFromEnd = new HashSet<>();
+		LinkedList<BFSNode> queueStart = new LinkedList<>(Arrays.asList(new BFSNode(startNode.get(), null)));
+		LinkedList<BFSNode> queueEnd = new LinkedList<>(Arrays.asList(new BFSNode(endNode.get(), null)));
+		while(!queueStart.isEmpty() && !queueEnd.isEmpty()) {
+			BFSNode head = queueStart.poll();
+			BFSNode tail = queueEnd.poll();
+					visitedFromStart.add(head.node);
+			visitedFromEnd.add(tail.node);
+			pathHead.add(head);
+			pathTail.add(tail);
+			if (visitedFromStart.contains(tail.node) || visitedFromEnd.contains(head.node))
+				return mergePaths(pathHead, pathTail);
+
+
+
+		}
+
+		throw new IllegalArgumentException("There is no path between given nodes...");
+	}
+
+	private static List<Integer> resolvePathFromTree(LinkedList<BFSNode> tree) {
 		Collections.reverse(tree);
-		Queue<BFSAncestryPreservingNode> queueTree = tree;
+		Queue<BFSNode> queueTree = tree;
 		List<Integer> path = new ArrayList<>();
-		BFSAncestryPreservingNode last = queueTree.poll();
+		BFSNode last = queueTree.poll();
 		path.add(last.node.id);
 		while(!queueTree.isEmpty()) {
 			while(!queueTree.peek().node.equals(last.ancestor)) {
@@ -51,31 +81,31 @@ public class Graphs {
 		return path.get(path.size() - 1).equals(end);
 	}
 
-	private static LinkedList<BFSAncestryPreservingNode> treePreservingBFS(Graph g, int start, int end) {
+	private static LinkedList<BFSNode> treePreservingBFS(Graph g, int start, int end) {
 		Optional<Graph.Node> rootNode = g.nodes.stream().filter(item -> item.id == start).findFirst();
-		LinkedList<BFSAncestryPreservingNode> bfsOrder = new LinkedList<>();
+		LinkedList<BFSNode> bfsOrder = new LinkedList<>();
 
 		if (! rootNode.isPresent())
 			throw new IllegalArgumentException("Specified root node does not exist.");
 
 		Set<Graph.Node> visitedNodes = new HashSet<>();
-		LinkedList<BFSAncestryPreservingNode> queue = new LinkedList<>();
-		queue.add(new BFSAncestryPreservingNode(rootNode.get(), null));
+		LinkedList<BFSNode> queue = new LinkedList<>();
+		queue.add(new BFSNode(rootNode.get(), null));
 		while (!queue.isEmpty()) {
-			BFSAncestryPreservingNode polledNode = queue.poll();
+			BFSNode polledNode = queue.poll();
 			bfsOrder.add(polledNode);
 			visitedNodes.add(polledNode.node);
 			List<Graph.Node> toBeQueued = polledNode.node.adjacentNodes.stream().filter(node -> !visitedNodes.contains(node) && !queue.stream().anyMatch(inner -> inner.node.equals(node))).collect(Collectors.toList());
 			if (toBeQueued.stream().anyMatch(node -> node.id == end)){
-				bfsOrder.add(new BFSAncestryPreservingNode(toBeQueued.stream().filter(node->node.id==end).findFirst().get(), polledNode.node));
+				bfsOrder.add(new BFSNode(toBeQueued.stream().filter(node->node.id==end).findFirst().get(), polledNode.node));
 				return bfsOrder;
 			}
-			toBeQueued.stream().forEach(node -> queue.add(new BFSAncestryPreservingNode(node, polledNode.node)));
+			toBeQueued.stream().forEach(node -> queue.add(new BFSNode(node, polledNode.node)));
 		}
 
 		throw new IllegalArgumentException("The end is not reachable from the start.");		
 	}
-
+		
 	private static List<Integer> fs(Graph g, int start, boolean dfs, boolean search, int end) {
 		Optional<Graph.Node> rootNode = g.nodes.stream().filter(item -> item.id == start).findFirst();
 		List<Integer> fsOrder = new ArrayList<>(g.nodes.size());
@@ -91,7 +121,7 @@ public class Graphs {
 			fsOrder.add(polledNode.id);
 			visitedNodes.add(polledNode);
 			List<Graph.Node> toBeQueued = polledNode.adjacentNodes.stream().filter(node -> !visitedNodes.contains(node) && !queue.contains(node)).collect(Collectors.toList());
-			if (search && toBeQueued.stream().filter(node -> node.id == end).findFirst().isPresent()){
+			if (search && toBeQueued.stream().anyMatch(node -> node.id == end)){
 				fsOrder.add(end);
 				return fsOrder;
 			}
@@ -109,11 +139,11 @@ public class Graphs {
 
 	}
 
-	private static class BFSAncestryPreservingNode {
+	private static class BFSNode {
 		private Graph.Node node;
 		private Graph.Node ancestor;
 
-		private BFSAncestryPreservingNode (Graph.Node node, Graph.Node ancestor) {
+		private BFSNode (Graph.Node node, Graph.Node ancestor) {
 			this.node = node;
 			this.ancestor = ancestor;
 		}
