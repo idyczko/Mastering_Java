6180749f48f99710cbaf48a214474a2bfea6e541
Dyczko Igor <dyczig@akros.ch>
2018-10-02 14:52:12 +0200

    Rideable camel violent encapsualtion security breach.

---
+++ OCP/rideable/Camel.java
@@ -0,0 +1,14 @@
+public class Camel implements Rideable {
+	int weight = 2;
+	public String getGait() { return " mph, lope";}
+	void go(int speed) {
+		++speed;
+		weight++;
+		int walkrate = speed * weight;
+		System.out.println(walkrate + getGait());
+	}
+
+	public static void main(String[] args) {
+		new Camel().go(8);
+	}
+}
---
+++ OCP/rideable/Rideable.java
@@ -0,0 +1,3 @@
+public interface Rideable {
+	String getGait();
+}
