a63057ed42b8394d83841d218aa1956c9fcc4a08
Dyczko Igor (IT-SWE-CC1-JV6) <igor.dyczko@sbb.ch>
2018-10-18 15:46:26 +0200

    Well... we did learn something, didn't we?

---
+++ OCP/stupid_java_intricacies/packages/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	
+	public static void main(String[] args) {
+	
+		java.util.Date date1 = new java.util.Date();
+	}
+}
---
+++ OCP/stupid_java_intricacies/underscores/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		int x = 10_1;
+		int y = 0xff;
+		int z = 0b00001000;
+		System.out.println(x + " " + y + " " + z);
+	}
+}
