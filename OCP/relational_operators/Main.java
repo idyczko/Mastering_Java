052869893d2a1f7c7f23f47dceecb623f18c4e55
Dyczko Igor (IT-SWE-CC1-JV6) <igor.dyczko@sbb.ch>
2018-10-19 14:22:53 +0200

    Relational operators with different primitiva type comparisons.

---
+++ OCP/relational_operators/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		long l = 12l;
+		double d = 13.5;
+		float f = 15.7f;
+		short s = 50;
+
+		System.out.println((x > l) + " " + (s > f) + " " + (d > l));
+	}
+}
