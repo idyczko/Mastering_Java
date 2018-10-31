0d6ab73cc68ee3887a0b49e6b09b7f420d8502e7
Dyczko Igor (IT-SWE-CC1-JV6) <igor.dyczko@sbb.ch>
2018-10-31 17:11:52 +0100

    Some more workout... Let's figure out how to merge all common paths at each level.

--- Google_Interview/single_error_typing_control/MyDetrie.java
+++ Google_Interview/single_error_typing_control/MyDetrie.java
@@ -3,14 +3,36 @@ import java.util.HashSet;
 
 public class DictionaryDetrie implements Detrie {
 	
-	private MyNode root = new ;
-	private MyNode masterLeaf;
+	private MyNode root = new MyNode();
+	private MyNode masterLeaf = new MyNode();
 
 	public MyNode getRoot() {return this.root;}
 	public MyNode getMasterLeaf() {return this.masterLeaf;};
 
 	public void insertWord(String word) {
-		
+	
+		MyNode parent = root;
+		MyNode newNode;
+		for (char c : word.toCharArray()) {
+			Set<MyNode> successors = parent.getSuccessors();
+			if (successors.contains(this.masterLeaf))
+				return;
+
+			Optional<MyNode> next = successors.stream().filter(node -> node.getChar().equals(c)).findAny();
+			if (next.isPresent()) {
+				parent = next.get();
+				continue;
+			}
+
+			newNode = new MyNode(){{this.c = c}};
+			newNode.getPredecessors().add(parent);
+			parent.getSuccessors().add(newNode);
+			parent = newNode;
+		}
+
+		if(newNode != null) {
+			newNode.getSuccessors().add();
+		}
 	}
 
 	public static class MyNode implements Node {
