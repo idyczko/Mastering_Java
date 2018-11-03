0aa87c384ba4fa684c7b143594a4382fe92221e7
idyczko <igor.dyczko@gmail.com>
2018-11-03 22:02:25 +0100

    Working rolling hash :D. Modulus Maxima!

--- Google_Interview/rabin_karp/PatternFinder.java
+++ Google_Interview/rabin_karp/PatternFinder.java
@@ -16,9 +16,13 @@ public class PatternFinder {
 		long sequenceHash = hash(sequence.substring(0, length).toCharArray());
 		log("Subsequence hash: " + sequenceHash);
 		int index = 0;
-		while (index < (sequenceChars.length - length)) {
+		while (index <= (sequenceChars.length - length)) {
 			if (patternHash == sequenceHash && compareSequences(patternChars, sequenceChars, index))
 				return index;
+
+			if (index == (sequenceChars.length - length))
+				return -1;
+
 			sequenceHash = rollHash(sequenceChars[index], sequenceChars[index + patternChars.length], sequenceHash, patternChars.length);
 			log("New subsequence hash: " + sequenceHash);
 			index++;
@@ -37,14 +41,21 @@ public class PatternFinder {
 	}
 
 	private static long rollHash(char oldLetter, char newLetter, long oldHash, int length) {
-		return (BIG_PRIME + ((((oldHash - (oldLetter * ((long) Math.pow(PRIME, length - 1)))) * PRIME) + newLetter) % BIG_PRIME)) % BIG_PRIME;
+		long factor = 1;
+		while (--length > 0){
+			factor *= PRIME;
+			factor %= BIG_PRIME;
+		}
+		long hash = ((oldHash - (oldLetter * factor)) * PRIME) + newLetter;
+		return ((hash % BIG_PRIME) + BIG_PRIME) % BIG_PRIME;
 	}
 
 	private static long hash(char[] word) {
 		long hash = 0;
 
-		for (int i = 0; i < word.length; i++) {
-			hash += word[i]*((int) Math.pow(PRIME, word.length - 1 - i));
+		for (int i = 0; i < word.length; i++){
+			hash *= PRIME;
+			hash += word[i];
 			hash %= BIG_PRIME;
 		}
 
