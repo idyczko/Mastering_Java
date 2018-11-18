594231962bb68ed50de5c671d85bad24d1d065df
idyczko <igor.dyczko@gmail.com>
2018-11-18 20:25:43 +0100

    Not working common ancestor... :(

--- Cracking_the_Coding_Interview/trees/binary_tree_common_ancestor/BST.java
+++ Cracking_the_Coding_Interview/trees/binary_tree_common_ancestor/BST.java
@@ -11,37 +11,48 @@ public class BST {
 		if (root == null)
 			return root = new Node(i);
 
-		Node parent = root;
-		while (true)
+		return insert(root, new Node(i));
 	}
 
-	public Node commonAncestor(Node p, Node q) {
-		if (p == null)
+	public Node insert(Node parent, Node newNode) {
+		boolean goLeft = parent.v >= newNode.v;
+		
+		if (goLeft && parent.l == null)
+			return parent.l = newNode;
+
+		if (!goLeft && parent.r == null)
+			return parent.r = newNode;
+
+		return insert(goLeft ? parent.l : parent.r, newNode);
+	}
+
+	public Node findCommonAncestor(Node p, Node q) {
+		if (p == null || q == null)
 			return null;
 		
 		if (covers(p, q))
 			return p;
 
-		return p.p == q ? p.p : commonAncestor(p.p, q);
+		return commonAncestor(p, q);
+	}
+
+	private Node commonAncestor(Node p, Node q) {
+		if (p.p == null)
+			return null;
+
+		if (covers(p.getSibling(), q))
+			return p.p;
+
+		return commonAncestor(p.p, q);
 	}
 
 	private boolean covers(Node covering, Node covered) {
+		if (covering == null)
+			return false;
 		if (covering == covered)
 			return true;
 
-
-	}
-
-	private Node insert (Node newNode, Node parent) {
-		boolean goLeft = newNode.v <= parent.v;
-		Node nextParent = goLeft ? parent.l : parent.p;
-		if (nextParent == null)
-			if (goLeft)
-				return parent.l = newNode;
-			else
-				return parent.r = newNode;
-
-		return insert(newNode, nextParent);
+		return covers(covering.l, covered) || covers(covering.r, covered);
 	}
 
 	public static class Node {
@@ -58,6 +69,13 @@ public class BST {
 			this.v = v;
 			this.l = l;
 			this.r = r;
+		}
+
+		public Node getSibling() {
+			if (this.p == null)
+				return null;
+
+			return this == this.p.r ? this.p.l : this.p.r;
 		}
 	}
 }
---
+++ Cracking_the_Coding_Interview/trees/binary_tree_common_ancestor/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		BST tree = new BST(10);
+		tree.insert(5);
+		tree.insert(1);
+		tree.insert(3);
+		tree.insert(11);
+		tree.insert(10);
+		tree.insert(211);
+		tree.insert(11);
+		BST.Node p = tree.insert(11);
+		tree.insert(21);
+		BST.Node q = tree.insert(13);
+		tree.insert(16);
+		tree.insert(15);
+
+		System.out.println("Common ancestor of p and q: " + tree.findCommonAncestor(p, q).v);
+	}
+}
