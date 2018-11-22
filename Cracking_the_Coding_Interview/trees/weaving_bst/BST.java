69819cdb811e3d72270eb9e824365f46fb3da2bd
idyczko <igor.dyczko@gmail.com>
2018-11-22 22:11:31 +0100

    First not tested draft of bottom up weaving.

--- Cracking_the_Coding_Interview/trees/weaving_bst/BST.java
+++ Cracking_the_Coding_Interview/trees/weaving_bst/BST.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class BST {
 	
@@ -10,6 +11,54 @@ public class BST {
 
 	public void weave() {
 		weave(this.root, new ArrayList<>(), new HashSet<>());
+	}
+
+	public void weaveAlt() {
+		allSequences(this.root).forEach(seq -> {
+			System.out.print("[ ");
+			seq.forEach(item -> System.out.print(item + " "));
+			System.out.print("]\n");
+		});
+	}
+
+	private ArrayList<LinkedList<Integer>> allSequences(Node node) {
+		if (node == null)
+			return new ArrayList<LinkedList<Integer>>();
+
+		ArrayList<LinkedList<Integer>> left = allSequences(node.l);
+		ArrayList<LinkedList<Integer>> right = allSequences(node.r);
+
+		return new ArrayList<>(weaveSequences(left, right).stream().peek(seq -> seq.addFirst(node.v)).collect(Collectors.toList()));
+	}
+
+	private ArrayList<LinkedList<Integer>> weaveSequences(ArrayList<LinkedList<Integer>> left, ArrayList<LinkedList<Integer>> right) {
+		if (left.isEmpty() || right.isEmpty()) {
+			return left.isEmpty() ? right : left;
+		}
+
+		ArrayList<LinkedList<Integer>> result = new ArrayList<>();
+		for (LinkedList<Integer> l : left)
+			for (LinkedList<Integer> r : right)
+				result.addAll(weave(l, r));
+
+		return result;
+	}
+
+	private ArrayList<LinkedList<Integer>> weave(LinkedList<Integer> l, LinkedList<Integer> r) {
+		if (l.isEmpty() || r.isEmpty())
+			return new ArrayList<> (Collections.singletonList(l.isEmpty() ? r : l));
+
+		ArrayList<LinkedList<Integer>> result = new ArrayList<>();
+		Integer lhead = l.getFirst();
+		l.removeFirst();
+		result.addAll(weave(l, r).stream().peek(s -> s.addFirst(lhead)).collect(Collectors.toList()));
+		l.addFirst(lhead);
+
+		Integer rhead = r.getFirst();
+		r.removeFirst();
+		result.addAll(weave(l, r).stream().peek(s -> s.addFirst(rhead)).collect(Collectors.toList()));
+		
+		return result;
 	}
 
 	private void weave(Node node, List<Integer> seq, Set<Node> moves) {
