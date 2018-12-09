cd7b69cd5754a32ec7c20d2aa7a3fa90eac46b4d
idyczko <igor.dyczko@gmail.com>
2018-12-09 17:09:34 +0100

    Wood 1 submission.

--- CodinGame/xmas_rush/Player.java
+++ CodinGame/xmas_rush/Player.java
@@ -15,6 +15,9 @@ class Player {
         String[][] tiles = new String[7][7];
         String pushMove = "";
         String move = "";
+        Map<String, Integer[]> myItems = new HashMap<>();
+        boolean init = true;
+
 
         // game loop
         while (true) {
@@ -40,34 +43,51 @@ class Player {
                 int itemPlId = in.nextInt();
                 items[itemPlId][0] = itemX;
                 items[itemPlId][1] = itemY;
+                if (itemPlId == 0)
+                    myItems.put(itemName, new Integer[] {itemX, itemY});
             }
             int numQuests = in.nextInt(); // the total number of revealed quests for both players
+            PriorityQueue<String> activeQuests = new PriorityQueue<>(6, new Comparator<String>() {
+
+                @Override
+                public int compare(String s1, String s2) {
+
+                  return distance(players[0][0], players[0][1], myItems.get(s1)[0], myItems.get(s1)[1])
+                            - distance(players[0][0], players[0][1], myItems.get(s2)[0], myItems.get(s2)[1]);
+                }
+            });
             for (int i = 0; i < numQuests; i++) {
                 String questItemName = in.next();
                 int questPlayerId = in.nextInt();
+                if (questPlayerId == 0)
+                    activeQuests.add(questItemName);
             }
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
+            int[] targetItem = new int[] {myItems.get(activeQuests.peek())[0], myItems.get(activeQuests.peek())[1]};
+            System.err.println("Target item: " + activeQuests.peek() + targetItem[0] + " " + targetItem[1]);
             if(turnType == 0)
-                pushMove = getToTheDockingPoint(tiles, players, items);
+                pushMove = getToTheDockingPoint(tiles, players, targetItem);
             else
-                move = calculateMove(tiles, players, items);
+                move = calculateMove(tiles, players, targetItem);
             System.out.println(turnType == 0 ? pushMove : (move == null ? "PASS" : move)); // PUSH <id> <direction> | MOVE <direction> | PASS
         }
     }
 
-    private static String getToTheDockingPoint(String[][] tiles, int[][] players, int[][] items) {
+    private static String getToTheDockingPoint(String[][] tiles, int[][] players, int[] item) {
+        if (item[0] < 0)
+            return movePlayerTowardsCenter(players);
         String playerTile = tiles[players[0][0]][players[0][1]];
-        String itemTile = tiles[items[0][0]][items[0][1]];
-        List<Integer[]> docks = getDockingPoints(tiles, players[0][0], players[0][1], items[0][0], items[0][1]);
+        String itemTile = tiles[item[0]][item[1]];
+        List<Integer[]> docks = getDockingPoints(tiles, players[0][0], players[0][1], item[0], item[1]);
         if (docks.isEmpty())
             return movePlayerTowardsCenter(players);
         for (Integer[] dock : docks)
             System.err.println("Found docking point: " + dock[0] + " " + dock[1]);
-        Integer[] closestDock = findClosestDock(docks, items[0][0], items[0][1]);
+        Integer[] closestDock = findClosestDock(docks, item[0], item[1]);
         System.err.println("Closest docking point: " + closestDock[0] + " " + closestDock[1]);
-        return calculateDockMove(closestDock, players, items);
+        return calculateDockMove(closestDock, players, item);
     }
 
     private static String movePlayerTowardsCenter(int[][] players) {
@@ -78,32 +98,32 @@ class Player {
             (players[0][0] + (yDiff > 0 ? " UP" : " DOWN")));
     }
 
-    private static String calculateDockMove(Integer[] closestDock, int[][] players, int[][] items) {
-      if (distance(items[0][0], items[0][1], closestDock[0], closestDock[1]) == 0)
+    private static String calculateDockMove(Integer[] closestDock, int[][] players, int[] item) {
+      if (distance(item[0], item[1], closestDock[0], closestDock[1]) == 0)
         return "PUSH " + ((closestDock[0] + 3) % 7) + " DOWN";
-      if (items[0][0] == players[0][0])
-        return escapeColumn(closestDock, items);
-      if (items[0][1] == players[0][1])
-        return escapeRow(closestDock, items);
+      if (item[0] == players[0][0])
+        return escapeColumn(closestDock, item);
+      if (item[1] == players[0][1])
+        return escapeRow(closestDock, item);
 
-      int xDiff = closestDock[0] - items[0][0];
-      int yDiff = closestDock[1] - items[0][1];
+      int xDiff = closestDock[0] - item[0];
+      int yDiff = closestDock[1] - item[1];
       boolean goHorizontal = abs(xDiff) != abs(yDiff) ? abs(xDiff) > abs(yDiff) :
-            (abs(items[0][1] - players[0][1]) == 1);
-      return "PUSH " + (goHorizontal ? (items[0][1] + (xDiff > 0 ? " RIGHT" : " LEFT")) :
-        (items[0][0] + (yDiff > 0 ? " DOWN" : " UP")));
+            (abs(item[1] - players[0][1]) == 1);
+      return "PUSH " + (goHorizontal ? (item[1] + (xDiff > 0 ? " RIGHT" : " LEFT")) :
+        (item[0] + (yDiff > 0 ? " DOWN" : " UP")));
     }
 
-    private static String escapeColumn(Integer[] closestDock, int[][] items) {
-      if (closestDock[0] == items[0][0])
-        return "PUSH " + items[0][1] + (closestDock[0] > 3 ? " LEFT" : " RIGHT");
-      return "PUSH " + items[0][1] + (closestDock[0] > items[0][0] ? " RIGHT" : " LEFT");
+    private static String escapeColumn(Integer[] closestDock, int[] item) {
+      if (closestDock[0] == item[0])
+        return "PUSH " + item[1] + (closestDock[0] > 3 ? " LEFT" : " RIGHT");
+      return "PUSH " + item[1] + (closestDock[0] > item[0] ? " RIGHT" : " LEFT");
     }
 
-    private static String escapeRow(Integer[] closestDock, int[][] items) {
-      if (closestDock[1] == items[0][1])
-        return "PUSH " + items[0][0] + (closestDock[1] > 3 ? " UP" : " DOWN");
-      return "PUSH " + items[0][0] + (closestDock[1] > items[0][1] ? " DOWN" : " UP");
+    private static String escapeRow(Integer[] closestDock, int[] item) {
+      if (closestDock[1] == item[1])
+        return "PUSH " + item[0] + (closestDock[1] > 3 ? " UP" : " DOWN");
+      return "PUSH " + item[0] + (closestDock[1] > item[1] ? " DOWN" : " UP");
     }
 
     private static Integer[] findClosestDock(List<Integer[]> docks, int x, int y) {
@@ -133,30 +153,30 @@ class Player {
         return docks;
     }
 
-    private static String keepItemClose(int[][] players, int[][] items) {
+    private static String keepItemClose(int[][] players, int[] item) {
 
-        if (items[0][0] < 0)
+        if (item[0] < 0)
             return "PUSH " + ((players[0][0] + 1)%7) + " DOWN";
-        int xDistance = players[0][0] - items[0][0];
-        int yDistance = players[0][1] - items[0][1];
+        int xDistance = players[0][0] - item[0];
+        int yDistance = players[0][1] - item[1];
         if (xDistance == 0 && abs(yDistance) != 1)
-            return "PUSH " + items[0][1] + " LEFT";
+            return "PUSH " + item[1] + " LEFT";
         if (yDistance == 0 && abs(xDistance) != 1)
-            return "PUSH " + items[0][1] + " DOWN";
+            return "PUSH " + item[1] + " DOWN";
         if (abs(xDistance) > 1)
-            return "PUSH " + items[0][1] + " " + (xDistance < 0 ? "LEFT" : "RIGHT");
+            return "PUSH " + item[1] + " " + (xDistance < 0 ? "LEFT" : "RIGHT");
         if (abs(yDistance) > 1)
-            return "PUSH " + items[0][0] + " " + (yDistance < 0 ? "UP" : "DOWN");
+            return "PUSH " + item[0] + " " + (yDistance < 0 ? "UP" : "DOWN");
 
         return "PUSH " + players[1][0] + " DOWN";
     }
 
-    private static String calculateMove(String[][] tiles, int[][] players, int[][] items) {
+    private static String calculateMove(String[][] tiles, int[][] players, int[] item) {
         int [][] visited = new int[7][7];
-        return calculateMove(tiles, players, items, players[0][0], players[0][1], "MOVE", visited);
+        return calculateMove(tiles, players, item, players[0][0], players[0][1], "MOVE", visited);
     }
 
-    private static String calculateMove(String[][] tiles, int[][] players, int[][] items,
+    private static String calculateMove(String[][] tiles, int[][] players, int[] item,
                                         int x, int y, String parent, int[][] visited) {
         if (visited[x][y] == 1)
             return null;
@@ -167,18 +187,18 @@ class Player {
         visited[x][y] = 1;
         System.err.println(x + " " + y + " " + tiles[x][y]);
 
-        if (items[0][0] == x && items[0][1] == y)
+        if (item[0] == x && item[1] == y)
             return parent;
 
         List<String> paths = new ArrayList<>();
         paths.add(tiles[x][y].charAt(3) == '1' ? ("RIGHT".equals(parent) ? null :
-            (x == 0 ? null : calculateMove(tiles, players, items, x - 1, y, "LEFT", visited))) : null);
+            (x == 0 ? null : calculateMove(tiles, players, item, x - 1, y, "LEFT", visited))) : null);
         paths.add(tiles[x][y].charAt(1) == '1' ? ("LEFT".equals(parent) ? null :
-            (x == 6 ? null : calculateMove(tiles, players, items, x + 1, y, "RIGHT", visited))) : null);
+            (x == 6 ? null : calculateMove(tiles, players, item, x + 1, y, "RIGHT", visited))) : null);
         paths.add(tiles[x][y].charAt(0) == '1' ? ("DOWN".equals(parent) ? null :
-            (y == 0 ? null : calculateMove(tiles, players, items, x, y - 1, "UP", visited))) : null);
+            (y == 0 ? null : calculateMove(tiles, players, item, x, y - 1, "UP", visited))) : null);
         paths.add(tiles[x][y].charAt(2) == '1' ? ("UP".equals(parent) ? null :
-            (y == 6 ? null : calculateMove(tiles, players, items, x, y + 1, "DOWN", visited))) : null);
+            (y == 6 ? null : calculateMove(tiles, players, item, x, y + 1, "DOWN", visited))) : null);
         String result = null;
         for (String path : paths) {
           if (path != null)
