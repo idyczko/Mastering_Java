9778676b3aadf1808d77bfdb64997cee26746665
igdy <igor.dyczko@gmail.com>
2018-12-12 12:13:31 +0100

    Pasting M into N.

--- Cracking_the_Coding_Interview/bit_ops/insert_int/Main.java
+++ Cracking_the_Coding_Interview/bit_ops/insert_int/Main.java
@@ -8,19 +8,41 @@ public class Main {
 		int m = Integer.valueOf(args[1]);
 		int j = Integer.valueOf(args[2]);
 
-		System.out.println("Bit lengths:\nn: " + bitLength(n) + "\nm: " + bitLength(m));
-		System.out.println(insert(n, m, j));
+		System.out.println("Bits:\nn: " + bitLength(n) + ": " + bits(n) + "\nm: " + bitLength(m) + ": " + bits(m));
+		System.out.println(bits(insert(n, m, j)));
 	}
 
 	private static int insert(int n, int m, int j) {
-		return 0;
+		if (bitLength(n) - j < bitLength(m))
+			throw new IllegalArgumentException("Paste not allowed.");
+		int left = 1;
+		while (bitLength(left) < j - 1)
+			left = (left << 1) | 1;
+		left <<= (bitLength(n) - bitLength(left));
+
+		System.out.println(bits(left));
+		int right = (int) Math.pow(2, (bitLength(n) - j - bitLength(m) + 1)) - 1;
+		System.out.println(bits(right));
+		int mask = left | right;
+		int masked = n & mask;
+		System.out.println(bits(masked));
+		int shifted = m << (bitLength(n) - j - bitLength(m));
+		System.out.println(m + " " + bits(shifted));
+		return masked | shifted;
 	}
 
 	private static int bitLength(int n) {
-		int length = 0;
+		int length = 1;
 		while ((n /= 2) != 0)
 			length++;
 
 		return length;
+	}
+
+	private static String bits(int n) {
+		StringBuilder sb = new StringBuilder(String.valueOf(n & 1));
+		while ((n /= 2) != 0)
+			sb.append(String.valueOf(n & 1));
+		return sb.reverse().toString();
 	}
 }
