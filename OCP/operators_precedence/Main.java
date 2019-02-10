66e0ca23daa6189d37b1df80970264e1705e88c1
idyczko <igor.dyczko@gmail.com>
2019-02-10 21:18:18 +0100

    StringBuilder tests.

---
+++ OCP/operators_precedence/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		int x = 10;
+		int y = x++ * x--;
+		int z = ++y * --y;
+		int w = --z * z++;
+		int c = 10;
+		int v = --c * c++;
+		System.out.println(y + " " + z + " " + w + " " + v);
+	}
+}
