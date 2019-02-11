4c9a939816898f32cc140beb3af105b33b579d9e
idyczko <igor.dyczko@gmail.com>
2019-02-11 20:59:41 +0100

    Stupid String equality.

---
+++ OCP/string/Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+
+	public static void main(String[] args) {
+	
+		String s = "Hello World";
+		String d = "Hello World".trim();
+		System.out.println((s == d) + " " + (s.trim() == d));
+	}
+}
