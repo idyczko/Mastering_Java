2a22405f8602fcf2e0dbf1e565c38295ccdb74b8
igdy <igor.dyczko@gmail.com>
2019-02-12 15:22:42 +0100

    Java boxed boolean tests.

---
+++ OCP/boxing/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Boolean b = null;
+		Boolean a = true;
+		Boolean c = false;
+		System.out.println((b == Boolean.TRUE) + " " + (Boolean.TRUE.equals(b)) + " " + (a == Boolean.TRUE));
+	}
+}
