f675234bddc48969957dea5ec4837d0853767f3e
igdy <igor.dyczko@gmail.com>
2019-02-12 12:17:55 +0100

    Strings... substring does not return the same reference, as trim does...

--- OCP/string/Main.java
+++ OCP/string/Main.java
@@ -3,8 +3,10 @@ class Main {
 
 	public static void main(String[] args) {
 	
+		String h = "Hello";
 		String s = "Hello World";
 		String d = "Hello World".trim();
-		System.out.println((s == d) + " " + (s.trim() == d));
+		System.out.println((s == d) + " " + (s.trim() == d) + " " + (h == s.substring(0, 5)));
+		System.out.println(h + " " + s.substring(0, 5) + " " + h.equals(s.substring(0, 5)));
 	}
 }
