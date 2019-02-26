51ff7ca782170e672e8721f2ade3d421964d6d0c
igdy <igor.dyczko@gmail.com>
2019-02-26 12:04:06 +0100

    Pizza Cleanup

--- Hash_Code/pizza/Main.java
+++ Hash_Code/pizza/Main.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
-public class Solution {
+public class Main {
 
+  private static final String TAG = "///////////////////////////////////// %s /////////////////////////////////////";
   private static int R;
   private static int C;
   private static int L;
@@ -10,11 +11,8 @@ public class Solution {
   private static boolean log = false;
 
   public static void main(String[] args) {
-    args = new String[4];
-    args[0]= "-l";
-    args[1]= "-s";
-    args[2]= "-p";
-    args[3]= "-sol";
+    if (option(args, "-l"))
+      log = true;
 
     Scanner in = new Scanner(System.in);
 
@@ -23,20 +21,36 @@ public class Solution {
     L = in.nextInt();
     H = in.nextInt();
     pizza = new char[R][C];
-    String line = in.nextLine();
-    for(int i = 0; i < R; i++) {
-      line = in.nextLine();
-      char[] chars = line.toCharArray();
-      for (int j = 0; j < chars.length; j++) {
-        pizza[i][j] = chars[j];
-      }
-    }
-    if (Arrays.asList(args).contains("-l"))
-      log = true;
+    readPizza(in);
 
-    Set<Slice> initSlices = computeInitSlices(pizza, L, H);
+    log(String.format(TAG, "Pizza"));
+    if (log) {
+      print(pizza);
+    }
+
+    Set<Slice> orderedPickSolution = createSolutionByOrderedPick();
+
+    log(String.format(TAG, "Ordered Pick Solution"));
+
+    if(log) {
+      orderedPickSolution.forEach(System.out::println);
+      char[][] slicedPizza = markPizza(orderedPickSolution);
+      print(slicedPizza);
+    }
+
+    Set<Slice> expansionSolution = searchSolutionSpaceByExpansion(orderedPickSolution);
+    log(String.format(TAG, "Expansion Solution"));
+    if(log) {
+      expansionSolution.forEach(System.out::println);
+      char[][] slicedPizzaSol = markPizza(expansionSolution);
+      print(slicedPizzaSol);
+    }
+
+  }
+
+  private static Set<Slice> createSolutionByOrderedPick() {
+    Set<Slice> initSlices = computeLimitedSlices(pizza, L, H, H);
     PriorityQueue<Slice> priorityQueue = new PriorityQueue<Slice>(R*C, Comparator.comparingInt(Slice::size));
-    System.out.println("///////////////////////////////");
     priorityQueue.addAll(initSlices);
 
     Set<Slice> solution = new HashSet<>(Arrays.asList(priorityQueue.poll()));
@@ -47,65 +61,35 @@ public class Solution {
         solution.add(slice);
     }
 
-    if (Arrays.asList(args).contains("-p"))
-      print(pizza);
-    if (Arrays.asList(args).contains("-s"))
+    if (log)
       initSlices.forEach(System.out::println);
 
-    System.out.println("///////////////////////////// Solution ////////////////////////");
-    if (Arrays.asList(args).contains("-sol")) {
-      solution.forEach(System.out::println);
-      char[][] slicedPizza = markPizza(solution);
-      print(slicedPizza);
-
-      Set<Slice> sol = searchSolutionSpace(solution);
-      System.out.println("///////////////////////////// Expanded Solution ////////////////////////");
-      sol.forEach(System.out::println);
-      char[][] slicedPizzaSol = markPizza(sol);
-      print(slicedPizzaSol);
-    }
-
-
+    return solution;
   }
 
-  private static char[][] markPizza(Set<Slice> solution) {
-
-    char[][] slicedPizza = pizza.clone();
-    int index = 0;
-    for (Slice slice : solution) {
-      index++;
-      for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
-          if (i >= slice.upperLeft.y && i <= slice.lowerRight.y && j >= slice.upperLeft.x && j <= slice.lowerRight.x) {
-            slicedPizza[i][j] = (char)('0'+ index);
-          }
-        }
-      }
-    }
-    return slicedPizza;
-  }
-
-  private static Set<Slice> searchSolutionSpace(Set<Slice> initSlices) {
-    Set<Slice> tabu = new HashSet<>();
+  private static Set<Slice> searchSolutionSpaceByExpansion(Set<Slice> initSlices) {
+    Set<Slice> nonExpandable = new HashSet<>();
     while (!initSlices.isEmpty()) {
-      Optional<Slice> sliceOp = initSlices.stream().findAny();
-      Slice slice = sliceOp.get();
+      Slice slice = chooseSliceForExpansion(initSlices);
       initSlices.remove(slice);
-      Slice expanded = expand(slice, tabu, initSlices);
+      Slice expanded = expand(slice, nonExpandable, initSlices);
+
       if (expanded.equals(slice)) {
-        tabu.add(slice);
+        nonExpandable.add(slice);
         continue;
       }
-      boolean intersects = intersectS(expanded, tabu, initSlices);
-      if (intersects)
-        tabu.add(slice);
-      else
-        initSlices.add(expanded);
+
+      initSlices.add(expanded);
     }
 
-    return tabu;
+    return nonExpandable;
   }
 
+  private static Slice chooseSliceForExpansion(Set<Slice> slices) {
+    if (slices.isEmpty())
+      throw new IllegalStateException("Trying to choose from empty set.");
+    return slices.stream().findAny().get();
+  }
 
   private static Slice expand(Slice slice, Set<Slice> s1, Set<Slice> s2) {
     Slice expanded = null;
@@ -114,96 +98,94 @@ public class Solution {
     } catch(IllegalStateException e) {
     }
 
-    if (expanded == null || intersectS(expanded, s1, s2)) {
+    if (expanded == null || intersect(expanded, s1, s2)) {
       try {
         expanded = slice.expandRight();
       } catch(IllegalStateException e) {
       }
     }
-    if (expanded == null || intersectS(expanded, s1, s2)) {
+    if (expanded == null || intersect(expanded, s1, s2)) {
       try {
         expanded = slice.expandUp();
       } catch(IllegalStateException e) {
       }
     }
-    if (expanded == null || intersectS(expanded, s1, s2)) {
+    if (expanded == null || intersect(expanded, s1, s2)) {
       try {
         expanded = slice.expandDown();
       } catch(IllegalStateException e) {
       }
     }
-    return expanded != null && !intersectS(expanded, s1, s2) ? expanded : slice;
+    return expanded != null && !intersect(expanded, s1, s2) ? expanded : slice;
   }
 
-  private static boolean intersectS(Slice slice, Set<Slice> s1, Set<Slice> s2) {
-    boolean intersects = s1.stream().anyMatch(s -> intersect(s, slice));
-    intersects |= s2.stream().anyMatch(s -> intersect(s, slice));
-    return intersects;
+  private static boolean intersect(Slice slice, Set... sets) {
+    for (Set<Slice> set : sets)
+      if (set.stream().anyMatch(s -> intersect(s, slice)))
+        return true;
+    return false;
   }
 
   private static Set<Slice> computeInitSlices(char[][] pizza, int L, int H) {
-    return computeMinimalSizeSlices(pizza, L, H);
+    return computeLimitedSlices(pizza, L, H, 2*L);
   }
 
-  private static Set<Slice> computeMinimalSizeSlices(char[][] pizza, int L, int H) {
+  private static Set<Slice> computeLimitedSlices(char[][] pizza, int L, int H, int limit) {
     Set<Slice> slices = new HashSet<>();
     for (int i = 0; i < R; i++) {
       for (int j = 0; j < C; j++) {
-        if (log)
-          System.out.println("Cell: " + (i*C + j) +"/" + (R*C));
-
-        slices.addAll(computeMinimalSlicesForCenter(i, j, pizza));
+        log("Cell: " + (i*C + j) +"/" + (R*C));
+        slices.addAll(generateLimitedSlicesForCenter(i, j, pizza, limit));
       }
     }
 
     return slices;
   }
 
+  private static Set<Slice> computeAllFeasibleSlicesForCenter(int i, int j, char[][] pizza) {
+    return generateLimitedSlicesForCenter(i, j, pizza, H);
+  }
+
   private static Set<Slice> computeMinimalSlicesForCenter(int i, int j, char[][] pizza) {
+    return generateLimitedSlicesForCenter(i, j, pizza, 2*L);
+  }
+
+  private static Set<Slice> generateLimitedSlicesForCenter(int i, int j, char[][] pizza, int limit) {
     Set<Slice> slices = new HashSet<>();
     Slice slice = new Slice(new Point(j, i), new Point(j, i));
-    computeRecursively(slice, slices);
+    expandSliceRecursively(slice, slices, limit);
     return slices;
   }
 
-  private static void computeRecursively(Slice slice, Set<Slice> slices) {
+  private static void expandSliceRecursively(Slice slice, Set<Slice> slices, int expansionLimit) {
     if (slice.size() >= 2*L && slice.isAllowed()) {
       slices.add(slice);
       return;
     }
-    if (slice.size() >= H)
+    if (slice.size() >= expansionLimit)
       return;
 
     try {
       Slice left = slice.expandLeft();
-      computeRecursively(left, slices);
+      expandSliceRecursively(left, slices, expansionLimit);
     } catch(IllegalStateException e) {
     }
     try {
       Slice right = slice.expandRight();
-      computeRecursively(right, slices);
+      expandSliceRecursively(right, slices, expansionLimit);
     } catch(IllegalStateException e) {
     }
     try {
       Slice up = slice.expandUp();
-      computeRecursively(up, slices);
+      expandSliceRecursively(up, slices, expansionLimit);
     } catch(IllegalStateException e) {
     }
     try {
       Slice down = slice.expandDown();
-      computeRecursively(down, slices);
+      expandSliceRecursively(down, slices, expansionLimit);
     } catch(IllegalStateException e) {
     }
 
-
-  }
-
-  private static void print(char[][] pizza) {
-    for (char[] row : pizza) {
-      for (char ing : row)
-        System.out.print(ing + " ");
-      System.out.println();
-    }
   }
 
   private static boolean intersect(Slice s1, Slice s2) {
@@ -235,6 +217,46 @@ public class Solution {
     return true;
   }
 
+  private static void readPizza(Scanner sc) {
+    String line = sc.nextLine();
+    for(int i = 0; i < R; i++) {
+      line = sc.nextLine();
+      char[] chars = line.toCharArray();
+      for (int j = 0; j < chars.length; j++) {
+        pizza[i][j] = chars[j];
+      }
+    }
+  }
+
+  private static char[][] markPizza(Set<Slice> solution) {
+    char[][] slicedPizza = pizza.clone();
+    int index = 0;
+    for (Slice slice : solution) {
+      index++;
+      for (int i = slice.upperLeft.y; i <= slice.lowerRight.y; i++)
+        for (int j = slice.upperLeft.x; j <= slice.lowerRight.x; j++)
+            slicedPizza[i][j] = (char)('0'+ index);
+
+    }
+    return slicedPizza;
+  }
+
+  private static boolean option(String[] args, String op) {
+    return Arrays.stream(args).anyMatch(a -> a.equals(op));
+  }
+
+  private static void print(char[][] pizza) {
+    for (char[] row : pizza) {
+      for (char ing : row)
+        System.out.print(ing + " ");
+      System.out.println();
+    }
+  }
+
+  private static void log(String str) {
+    if (log)
+      System.out.println(str);
+  }
 
   private static class Slice {
     Point upperLeft;
