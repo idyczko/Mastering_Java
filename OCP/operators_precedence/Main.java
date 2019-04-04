f888195674459cbd77bf18be6065eba6949ffef1
igdy <igor.dyczko@gmail.com>
2019-04-05 00:06:53 +0200

    OCA training.

--- OCP/operators_precedence/Main.java
+++ OCP/operators_precedence/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		int w = --z * z++;
 		int c = 10;
 		int v = --c * c++;
-		System.out.println(y + " " + z + " " + w + " " + v);
+		System.out.println(y + " " + z + " " + w + " " + v + " " + (false|true));
+
 	}
 }
