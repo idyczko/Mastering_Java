35a4dcf3328ea6f8beab0befe67b649017708f5d
idyczko <igor.dyczko@gmail.com>
2019-04-09 00:31:17 +0200

    OCA...

--- OCP/trash/Main.java
+++ OCP/trash/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
@@ -7,11 +8,28 @@ public class Main {
 		List<Integer> list = new ArrayList<>();
 		short s = 10;
 		list.add((int) s);
+		A a = new A();
+		B b = new B();
+		A ab = new B();
+		Object o = true ? "DASDSA" : 10;
+		try {
+		a.meth();
+		a.meth2();
+		b.meth();
+		b.meth2();
+		ab.meth();
+		ab.meth2();
+		} catch (IOException e) {
+		} catch (RuntimeException e) {
+		} catch (Exception e) {}
 	}
 }
 
 class A {
-	void meth() {}
+	void meth() throws IOException{}
+	void meth(int i) {}
+	void meth(String s) {}
+	void meth2() {}
 }
 
 class B extends A {
