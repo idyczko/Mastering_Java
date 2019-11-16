5bf332ca07c35e09e6339a3c78efdd2072a8387d
igdy <igor.dyczko@gmail.com>
2019-11-16 23:08:50 +0100

    Hanoi printing

--- Cracking_the_Coding_Interview/recurrsion/hanoi/Hanoi.java
+++ Cracking_the_Coding_Interview/recurrsion/hanoi/Hanoi.java
@@ -3,38 +3,49 @@ import java.util.*;
 
 public class Hanoi {
 
-	
+	private static LinkedList hanoi[] = new LinkedList[3];
+
 	public static void main(String[] args) {
-		Stack<Integer> stack1 = new Stack<Integer>();
-		stack1.push(5);
-		stack1.push(4);
-		stack1.push(3);
-		stack1.push(2);
-		stack1.push(1);
-		Stack<Integer> stack2 = new Stack<Integer>();
-		Stack<Integer> stack3 = new Stack<Integer>();
-		Stack hanoi[] = {stack1, stack2, stack3};
+		LinkedList<Integer> stack1 = new LinkedList<Integer>();
+		stack1.addFirst(5);
+		stack1.addFirst(4);
+		stack1.addFirst(3);
+		stack1.addFirst(2);
+		stack1.addFirst(1);
+		LinkedList<Integer> stack2 = new LinkedList<Integer>();
+		LinkedList<Integer> stack3 = new LinkedList<Integer>();
+		hanoi[0] = stack1;
+		hanoi[1] = stack2;
+		hanoi[2] = stack3;
+		print();
 		transfer(hanoi);
 
-		while(!stack3.isEmpty())
-			System.out.println(stack3.pop());
 	}
 
-	private static void transfer(Stack<Integer> hanoi[]) {
+	private static void transfer(LinkedList<Integer> hanoi[]) {
 		transfer(hanoi[0].size(), hanoi[0], hanoi[1], hanoi[2]);
 	}
 
-	private static void transfer(int elements, Stack<Integer> from, Stack<Integer> buffer, Stack<Integer> to) {
+	private static void transfer(int elements, LinkedList<Integer> from, LinkedList<Integer> buffer, LinkedList<Integer> to) {
 		if (elements == 1) {
-			to.push(from.pop());
+			to.addFirst(from.removeFirst());
+			print();
 			return;
 		}
 		
 		transfer(elements - 1, from, to, buffer);
-		Integer top = from.pop();
-		to.push(top);
+		to.addFirst(from.removeFirst());
+		print();
 		transfer(elements - 1, buffer, from, to);
 	}
 
-
+	private static void print() {
+		int elements = hanoi[0].size() + hanoi[1].size() + hanoi[2].size();
+		System.out.println("=========================================================");
+		for (int i = elements - 1; i >= 0; i--) {
+			for (LinkedList list : hanoi)
+				System.out.print(list.size() - 1 >= i ? list.get(list.size() - i - 1) +"\t" : " \t");
+			System.out.println();
+		}
+	}
 }
